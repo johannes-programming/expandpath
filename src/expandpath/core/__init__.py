d72c08073b933dfb311e80368d9e694d7c89d3de
Johannes <johannes.programming@gmail.com>
2025-08-19 05:50:50 +0200

    Version 0.0.1

--- src/expandpath/core/__init__.py
+++ src/expandpath/core/__init__.py
@@ -1,10 +1,25 @@
-from typing import *
 import os
+from typing import *
 
-__all__ = ["expandpath"]
+import click
+import preparse
 
-def expandpath(value:Any, /)->Any:
+__all__ = ["expandpath", "main"]
+
+
+def expandpath(value: Any, /) -> Any:
     "This function expands a given path."
+    ans: Any
     ans = os.path.expanduser(value)
     ans = os.path.expandvars(ans)
     return ans
+
+
+@preparse.PreParser().click()
+@click.command(add_help_option=False)
+@click.help_option("-h", "--help")
+@click.version_option(None, "-V", "--version")
+@click.argument("path", type=str)
+def main(path: str) -> None:
+    "This command expands the given path."
+    click.echo(expandpath(path))
